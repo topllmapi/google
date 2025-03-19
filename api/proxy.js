8c23ab458f72cab545df8d8e3e9bc9db94cdaa71
topllmapi <llmapi.top@hotmail.com>
2025-03-19 21:38:12 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.wikipedia.org/";//your website url
+  let target = "https://gemini.google.com/app/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
