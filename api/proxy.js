51b079f5099d19913c068c4ff53c81a0efda1d47
topllmapi <llmapi.top@hotmail.com>
2025-03-19 21:21:57 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://search.llmapi.top/";//your website url
+  let target = "https://bing.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
